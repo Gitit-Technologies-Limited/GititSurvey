5c0816eb994701b1a54f164e24830970eba47d3d
noahbuilds <edetnoah@gmail.com>
2025-02-18 12:29:19 +0100

    --no-edit

--- application/migrations/m250205_092215_modify_last_edited_column.php
+++ application/migrations/m250205_092215_modify_last_edited_column.php
@@ -4,6 +4,13 @@ class m250205_092215_modify_last_edited_column extends CDbMigration
 {
     public function up()
     {
+        // Check if the column already exists
+        $table = Yii::app()->db->schema->getTable('{{surveys}}');
+        if (!isset($table->columns['last_edited'])) {
+            $this->addColumn('{{surveys}}', 'last_edited', 'DATETIME');
+        }
+
+        // Modify the column to have default CURRENT_TIMESTAMP and auto-update
         $this->execute("
             ALTER TABLE {{surveys}} 
             MODIFY last_edited DATETIME DEFAULT CURRENT_TIMESTAMP 
@@ -13,10 +20,13 @@ class m250205_092215_modify_last_edited_column extends CDbMigration
 
     public function down()
     {
-
-        $this->execute("
-            ALTER TABLE {{surveys}} 
-            MODIFY last_edited DATETIME NULL;
-        ");
+        // Revert to NULL if the column exists
+        $table = Yii::app()->db->schema->getTable('{{surveys}}');
+        if (isset($table->columns['last_edited'])) {
+            $this->execute("
+                ALTER TABLE {{surveys}} 
+                MODIFY last_edited DATETIME NULL;
+            ");
+        }
     }
 }
